b2f39bd0828bc6e6a7372919388d9cf819685aac
psixp <pablo_six@live.com>
2022-03-28 20:17:26 -0300

    intregrando todos os modulos à calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,10 @@
 const multiplicar = require('./modules/multiplicar');
 const dividir = require('./modules/dividir');
+const soma = require('./modules/soma')
+const subtrair = require('./modules/subtrair')
 
 
 console.log("Realizando uma multiplicação: ", multiplicar(3,3));
 console.log("Realizando uma divisão: ", dividir(10,2));
+console.log("Realizando uma soma: ", soma(6,6));
+console.log("Realizando uma subtração: ", dividir(10,8));
